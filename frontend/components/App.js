ff82bee4bc1878f505f3d0b81f3727c691333c49
Paul Maloney <paul@paulmaloney.com>
2024-04-28 15:49:24 -0700

    completed project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,11 +7,52 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [peopleData, setPeopleData] = useState([])
+  const [planetData, setPlanetData] = useState([])
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    axios.get(`http://localhost:9009/api/people`)
+      .then(res => {
+        setPeopleData(res.data)
+      })
+      .catch(err => {
+        console.log(err.data)
+      })
+    axios.get(`http://localhost:9009/api/planets`)
+      .then(res => { 
+        setPlanetData(res.data)
+      })
+      .catch(err => { 
+        console.log(err.data)
+      })
+    }, [])
+
+    const toggleHomeworldVisible = (personID) => {
+      console.log("clicked")
+      setPeopleData(currentPeople => 
+        currentPeople.map(
+          person => person.id === personID 
+          ? {...person, homeworldVisible: !person.homeworldVisible} 
+          : person
+        )
+      )
+    }
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
+      {peopleData.map(person => {
+        const homeWorldObj = planetData.find(planet => planet.id === person.homeworld)
+
+        return <Character 
+          key={person.id}
+          person={{...person, homeWorldObj}}
+          homeworldVisible={person.homeworldVisible}
+          toggleHomeworldVisible={() => toggleHomeworldVisible(person.id)}
+        />
+      })}
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
     </div>
   )
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+function Character(props) { // ❗ Add the props
+  const {person, homeworldVisible, toggleHomeworldVisible }  = props
+  
+  // // ❗ Create a state to hold whether the homeworld is rendering or not
+  
+
+  // // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
+    <div className='character-card'  onClick={toggleHomeworldVisible}>
+      <h3 className='character-name'>{person.name}</h3>
       {/* Use the same markup with the same attributes as in the mock */}
+      {homeworldVisible && <p>Planet: <span className='character-planet'>{person.homeWorldObj.name}</span></p>}
     </div>
-  )
-}
+    )
+  }
 
 export default Character
